9b3346584d9d89baf13ffa99400281b94dfcbd8a
Kristin Barr <kristinbarr@gmail.com>
2019-06-12 02:59:14 -0400

    closure solution update

--- assignments/closure.js
+++ assignments/closure.js
@@ -10,9 +10,9 @@ function funWithClosure () {
 }
 
 const secret = funWithClosure()
-console.log(secret())
-console.log(secret())
-console.log(secret())
+console.log(secret()) // I have 1 gummy bears!
+console.log(secret()) // I have 2 gummy bears!
+console.log(secret()) // I have 3 gummy bears!
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -23,51 +23,63 @@ const counter = () => {
   let count = 0
   return function() {
     count += 1
-    console.log(count)
     return count
   }
 };
 
-const newCounter = counter()
-
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter()
+console.log('newCounter', newCounter()) // 1
+console.log('newCounter', newCounter()) // 2
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 
-const counterFactory = (action) => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+
+const counterFactory = () => {
+
   let counter = 0
-
-  let obj = {
-    increment() {
-      counter += 1
-      return counter
-    },
-    decrement() {
-      counter -= 1
-      return counter
-    }
+  return {
+    increment: () => ++counter,
+    decrement: () => --counter,
   }
+}
 
-  if (action === 'increment') {
-    return obj.increment
-  }
-  if (action === 'decrement') {
-    return obj.decrement
-  }
+let myCounter = counterFactory()
 
-};
+console.log('count up!', myCounter.increment()) // 1
+console.log('count up!', myCounter.increment()) // 2
+console.log('count down!', myCounter.decrement()) // 1
+console.log('count down!', myCounter.decrement()) // 0
 
-const inc = counterFactory('increment')
-const dec = counterFactory('decrement')
-console.log(inc())
-console.log(inc())
-console.log(inc())
-console.log(dec())
-console.log(dec())
-console.log(dec())
+// const counterFactory = (action) => {
+//   let counter = 0
+//   let obj = {
+//     increment() {
+//       counter += 1
+//       return counter
+//     },
+//     decrement() {
+//       counter -= 1
+//       return counter
+//     }
+//   }
+//   if (action === 'increment') {
+//     return obj.increment
+//   }
+//   if (action === 'decrement') {
+//     return obj.decrement
+//   }
+// };
+
+// const inc = counterFactory('increment')
+// const dec = counterFactory('decrement')
+// console.log(inc())
+// console.log(inc())
+// console.log(dec())
+// console.log(dec())
+
+
